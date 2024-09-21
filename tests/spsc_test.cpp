68775babb072f2ee61766bc341bad9b81cf0e54b
ali_gewei <grey@pku.edu.cn>
2024-09-22 01:20:47 +0800

    finish first spsc test

--- tests/spsc_test.cpp
+++ tests/spsc_test.cpp
@@ -1,10 +1,31 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "spsc.hpp"
+
 #include <doctest/doctest.h>
 
-int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+#include <format>
+#include <optional>
+#include <thread>
 
 TEST_CASE("testing the factorial function") {
-    CHECK(factorial(1) == 1);
-    CHECK(factorial(2) == 2);
-    CHECK(factorial(3) == 6);
+    lockfree::SPSC<int, 1024> queue;
+
+    std::jthread producer{[&queue] {
+        for (auto i = 0; i < 10; ++i) {
+            while (!queue.push(i)) {
+                std::cout << "full, cannot push\n";
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    }};
+    std::jthread consumer{[&queue] {
+        while (true) {
+            std::optional<int> value;
+            while (!(value = queue.pop())) {
+                std::cout << "empty, cannot pop\n";
+                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+            }
+            std::cout << std::format("consumer got {}\n", *value);
+        }
+    }};
 }
