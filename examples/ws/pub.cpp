0a69116bb6456739e2adc3e8326674cd9d6e7d27
ali_gewei <grey@pku.edu.cn>
2024-10-09 13:24:08 +0800

    fix ClientManager

--- examples/ws/pub.cpp
+++ examples/ws/pub.cpp
@@ -5,34 +5,56 @@
 #include <hv/http_content.h>
 
 #include <chrono>
+#include <cstddef>
 #include <format>
 #include <fstream>
+#include <iostream>
 #include <mutex>
 #include <shared_mutex>
 #include <thread>
-#include <unordered_set>
+#include <unordered_map>
 
 // Thread-safe container for clients
 class ClientManager {
     mutable std::shared_mutex mutex_;
-    std::unordered_set<WebSocketChannelPtr> clients_;
+    std::unordered_map<WebSocketChannelPtr, size_t> channels_;  // {channel, client_id}
+    std::unordered_map<std::string, size_t> clients_map_;       // {name, client_id}
 
    public:
-    void add(WebSocketChannelPtr const& channel) {
+    void add(WebSocketChannelPtr const& channel, std::string const& name) {
         std::unique_lock lock(mutex_);
-        clients_.emplace(channel);
+        channels_[channel] = retrive_id(name);
     }
 
     void remove(WebSocketChannelPtr const& channel) {
         std::unique_lock lock(mutex_);
-        clients_.erase(channel);
+        channels_.erase(channel);
     }
 
     // Allows safe iteration over clients without copying
     void for_each_client(auto&& func) const {
         std::shared_lock lock(mutex_);
-        for (const auto& client : clients_) {
+        for (const auto& [client, _] : channels_) {
             func(client);
+        }
+    }
+
+    // find client_id in the clients_map_
+    size_t retrive_id(std::string const& name) {
+        if (clients_map_.contains(name)) {
+            return clients_map_[name];
+        }
+        return SIZE_MAX;
+    }
+
+    // load registered clients from json file
+    void load_clients() {
+        std::ifstream fin{"clientdb.json"};
+        auto j = hv::Json::parse(fin);
+        for (auto&& client : j["clients"]) {
+            std::string name = client["name"];
+            size_t id = client["id"];
+            clients_map_[name] = id;
         }
     }
 };
@@ -44,14 +66,16 @@ int main(int argc, char** argv) {
     int port = j["port"];
 
     ClientManager cm;
+    cm.load_clients();
     WebSocketService ws;  // default ping interval is disabled
     ws.onopen = [&cm](WebSocketChannelPtr const& channel, const HttpRequestPtr& req) {
-        cm.add(channel);
-        printf("onopen: GET %s\n", req->Path().c_str());
+        auto name = req->GetParam("name", "foo");
+        cm.add(channel, name);
+        std::cout << std::format("client {} connected {}\n", name, req->Path());
     };
     ws.onclose = [&cm](WebSocketChannelPtr const& channel) {
         cm.remove(channel);
-        printf("onclose\n");
+        std::cout << "client disconnected\n";
     };
 
     hv::WebSocketServer server{&ws};
